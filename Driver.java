9c69f5c567f965256b57b16b1c4005830c026228
Kurk Wei Yi <weiyikurk@gmail.com>
2020-10-14 21:16:50 +0800

    First commit exercise 1

---
+++ Driver.java
@@ -0,0 +1,19 @@
+public class Driver{
+
+	public static void main(String [] arg){
+
+		Fish nemo = new Fish();
+
+		nemo.Swim();
+		nemo.color = "red";
+		nemo.Swim();
+
+		Fish dory = new Fish();
+		dory.Swim();
+		dory.color= "yellow";
+		dory.Swim();
+
+	}
+
+
+}
